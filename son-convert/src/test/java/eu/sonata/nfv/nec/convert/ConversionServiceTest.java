b3dbaf4d9fe42188211b9283a933edf7a486b2c2
Michael Bredel <michael@bredel.at>
2016-07-04 16:45:43 +0200

    Modified license header to mention SONATA.

--- son-convert/src/test/java/eu/sonata/nfv/nec/convert/ConversionServiceTest.java
+++ son-convert/src/test/java/eu/sonata/nfv/nec/convert/ConversionServiceTest.java
@@ -1,9 +1,20 @@
 /*
- * Copyright (C) 2016, NEC, SONATA-NFV
- * All rights reserved.
+ * Copyright (C) 2015, SONATA-NFV, NEC
+ * ALL RIGHTS RESERVED
  *
  * This software may be modified and distributed under the terms
  * of the BSD license. See the LICENSE file for details.
+ *
+ * Neither the name of the SONATA-NFV and NEC nor the names of 
+ * its contributors may be used to endorse or promote products 
+ * derived from this software without specific prior written 
+ * permission.
+ * 
+ * This work has been performed in the framework of the SONATA project,
+ * funded by the European Commission under Grant number 671517 through 
+ * the Horizon 2020 and 5G-PPP programmes. The authors would like to 
+ * acknowledge the contributions of their colleagues of the SONATA 
+ * partner consortium (www.sonata-nfv.eu).
  */
 package eu.sonata.nfv.nec.convert;
 
